cc1427fee96c1b971a020cf5c38749e2c3fcfc26
vishnuram <vishnumurali9762@gmail.com>
2020-02-19 12:40:55 +0530

    inital project setup

---
+++ src/test/java/com/ce/forim/Forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ce.forim.Forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
